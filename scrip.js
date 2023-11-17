0954cab69946946e9c651b01b39e657889171bb0
Ann2123 <fortisregina@gmail.com>
2023-11-17 11:24:32 -0400

    se termino el reproductor

--- scrip.js
+++ scrip.js
@@ -9,18 +9,36 @@ axios.get('https://leonardoapi.onrender.com/music')
 
     //creo el componente     
         let div = document.createElement('div')
-        div.classList.add('musica')
+        div.classList.add('alinear') //cñase para estilos
+
+     div.addEventListener ('click', () => {
+
+        console.log ('Hizo click')
+
+        document.querySelector('#current_song_title').
+        innerHTML =song.title
+
+
+        document.querySelector('#current_song_author').
+        innerHTML =song.author
+
+        document.querySelector('#current_song_img').setAttribute('src', song.path.front)
+
+        document.querySelector('#imagen_principal').setAttribute('src', song.path.front)
+
+        document.querySelector('#current_song_audio').setAttribute('src', song.path.audio)
+
+    })
 
 
     //agrego el contenido al componente     
-        track_list.innerHTML += `
+        div.innerHTML += `
     
-        <div class="alinear">
             <img src="image/Frame (1).svg" alt="">
             <h3>${song.title}</h3>
             <p>${song.duration}</p>
             <img src="image/image 8.svg" alt="">
-        </div>
+        
         
         `
         
